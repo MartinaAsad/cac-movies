919f004bc60595510e9360040baeb76f40063866
MartinaAsad <lasupermartu15@gmail.com>
2024-05-28 12:20:37 -0300

    borre todo

--- script.js
+++
@@ -1,8 +0,0 @@
-//validaciones en inicio de sesion
-function validarInicioSesion(){
-    let inputEmail = document.getElementById("campoEmail").value;
-    let inputContra = document.getElementById("campoContra").value;
-
-    inputEmail!="" && inputContra !="" ?  location.href = "index.html": alert("Completar campos vacios");
-
-}
